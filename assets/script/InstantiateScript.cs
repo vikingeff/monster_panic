08da9e49aee633c1a5b1f7b3a2d75a997a131639
vikingeff <mail.leger@gmail.com>
2014-06-21 17:37:43 +0200

    bring game back

---
+++ assets/script/InstantiateScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateScript : MonoBehaviour {
+
+	public GameObject monster;
+	public int count = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0) && count == 0)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+			RaycastHit hit;
+            if (Physics.Raycast (ray, out hit, 500f)) 
+            {
+            	if (hit.collider.CompareTag("Ground"))
+                {
+                	Vector3 pos = hit.point;
+                	pos.y = 10;
+					Instantiate(monster, pos, Quaternion.Euler(0, 0, 0));
+					count++;
+				}
+            }
+		}
+	}
+}
---
+++ assets/script/LookAtScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtScript : MonoBehaviour {
+
+	public Transform cam;
+	public Vector3 diff;
+	public bool collide;
+
+	// Use this for initialization
+	void Start () {
+		collide = false;
+		cam = Camera.main.transform;
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(cam);
+		if (collide)
+			cam.position = new Vector3(transform.position.x, 0, transform.position.z) - diff;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+        if (collision.collider.CompareTag("Ground") && !collide)
+        {
+
+        	diff = transform.position - cam.position;
+        	collide = true;
+        	rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        	rigidbody.useGravity = false;
+        	rigidbody.drag = 1000;
+        	rigidbody.velocity = Vector3.zero;
+        }
+    }
+}
